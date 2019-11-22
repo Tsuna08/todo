bb084d33196f7dad6f3e1e280753757785db190d
Tsuna08 <Tsuna08@users.noreply.github.com>
2019-11-23 01:04:28 +0300

    Новые компоненты

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = () => {
+  return(
+    <h1> ToDo list</h1>
+    )
+}
+const Search = () => {
+  return(
+    <input placeholder='search'/>
+    )
+}
+const ToDoList = () => {
+  return(
+    <ul>
+      <li> Learn React </li>
+    </ul>
+    )
+}
+const App = () => {
+  return(
+    <div>
+      <Header/>
+      <Search/>
+      <ToDoList/>
+    </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
