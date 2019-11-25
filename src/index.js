aa51f3e0eec26f1eee48b8d079402350b0de06e9
Tsuna08 <Tsuna08@users.noreply.github.com>
2019-11-26 01:00:32 +0300

    Немного сss

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import Header from './components/Header.js'
 import Search from './components/Search.js'
 import ToDoList from './components/ToDoList/ToDoList.js'
 
+import './index.sass'
+
 const toDoData = [
   {label: 'Learn React', important: false, id: 1},
   {label: 'Drink coffee', important: false, id: 2}
@@ -12,7 +14,7 @@ const toDoData = [
 
 const App = () => {
   return(
-    <div>
+    <div className='app'>
       <Header/>
       <Search/>
       <ToDoList toDo={toDoData}/>
