d7e728410542a1ec3f41a64743735347119c5e10
Tsuna08 <Tsuna08@users.noreply.github.com>
2019-11-27 01:09:15 +0300

    Начало обработок событий

--- src/components/FilterButton/FilterButton.js
+++ src/components/FilterButton/FilterButton.js
@@ -6,15 +6,15 @@ import './FilterButton.sass'
 export default class FilterButton extends Component {
   render() {
     return(
-      <div class="button-group btn-group btn-group-toggle" data-toggle="buttons">
-        <label class="btn btn-outline-primary active">
-          <input type="radio" name="options" id="all" autocomplete="off" checked/> All
+      <div className="button-group btn-group btn-group-toggle" data-toggle="buttons">
+        <label className="btn btn-outline-primary active">
+          <input type="radio" name="options" id="all" /> All
         </label>
-        <label class="btn btn-outline-primary">
-          <input type="radio" name="options" id="active" autocomplete="off"/> Active
+        <label className="btn btn-outline-primary">
+          <input type="radio" name="options" id="active" /> Active
         </label>
-        <label class="btn btn-outline-primary">
-          <input type="radio" name="options" id="done" autocomplete="off"/> Done
+        <label className="btn btn-outline-primary">
+          <input type="radio" name="options" id="done" /> Done
         </label>
       </div>
     )
--- src/components/ToDoListItem/ToDoListItem.js
+++ src/components/ToDoListItem/ToDoListItem.js
@@ -3,11 +3,26 @@ import React, {Component} from 'react';
 import './ToDoListItem.sass'
 
 class ToDoListItem extends Component {
+  // Что бы передать this в функцию можно делать так: 
+  
+  // constructor() {
+  //   super()
+  //   this.onLabelClick = () => {
+  //     console.log(`Done: ${this.props.label}`)
+  //   }
+  // }
+
+  // А можно так. Этот вариант мне больше нравится
+  onLabelClick = () => { console.log(`Done: ${this.props.label}`) }
+  
   render() {
     const {label, important = false} = this.props
-
     return(
-      <span className={important ? 'important' : null}>{label}</span>
+      <span 
+        className={important ? 'important' : null} 
+        onClick={this.onLabelClick}>
+        {label}
+      </span>
     )
   }
 }
