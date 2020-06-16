378f7f5bb6644467ca9f781630652f2d0526ef77
Tsuna <beatles1964@yandex.ru>
2020-06-17 00:51:29 +0300

    Работа с состояниями

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,32 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import ToDoListItem from '../ToDoListItem/ToDoListItem.js'
-import './ToDoList.sass'
+import ToDoListItem from "../ToDoListItem/ToDoListItem.js";
+import "./ToDoList.sass";
 
-const ToDoList = ({toDo}) => {
-  const elements = toDo.map(item => {
-    const { id, ...itemProps } = item
-    
+const ToDoList = ({ toDo }) => {
+  const elements = toDo.map((item) => {
+    const { id, ...itemProps } = item;
+
     return (
-      <li key={item.id} className='list-group-item'>
-        <ToDoListItem { ... itemProps } />
-        <div className='buttonGroup'>
-          <button className='button btn btn-outline-success'>
-            <i className='fa fa-exclamation'></i>
-          </button>
-          <button className='button btn btn-outline-danger'>
-            <i className='fa fa-trash-o'></i>
-          </button>
-        </div>
+      <li key={item.id} className="list-group-item">
+        <ToDoListItem {...itemProps} />
       </li>
-      )
-    })
-  
-  return( 
-    <ul className='list-group toDoList'>
-      {elements}
-    </ul>
-  ) 
-}
+    );
+  });
 
-export default ToDoList
+  return <ul className="list-group toDoList">{elements}</ul>;
+};
+
+export default ToDoList;
--- src/components/ToDoListItem/ToDoListItem.js
+++ src/components/ToDoListItem/ToDoListItem.js
@@ -1,34 +1,62 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import './ToDoListItem.sass'
+import "./ToDoListItem.sass";
 
 class ToDoListItem extends Component {
-  // Что бы передать this в функцию можно делать так: 
+  // Что бы передать this в функцию можно делать так:
   // constructor() {
   //   super()
   //   this.onLabelClick = () => {
   //     console.log(`Done: ${this.props.label}`)
   //   }
   // }
-  state = { done: false }
+  state = { done: false, important: false };
   // А можно так. Этот вариант мне больше нравится
-  onLabelClick = () => this.setState({ done: true })  
+  onLabelClick = () => {
+    // Вызывается тогда, когда стейт готов
+    this.setState(({ done }) => {
+      return { done: !done };
+    });
+  };
+  onMarkImportant = () => {
+    this.setState((state) => {
+      return { important: !state.important };
+    });
+  };
   render() {
-    const { label, important = false } = this.props
-    const { done } = this.state
+    const { label } = this.props;
+    const { done, important } = this.state;
 
-    let className = ''
-    if(done) className += 'done '
-    if(important) className += 'important '
-    
-    return(
-      <span 
-        className={className} 
-        onClick={this.onLabelClick}>
-        {label}
-      </span>
-    )
+    let className = "";
+    if (done) {
+      className += "done ";
+    }
+    if (important) {
+      className += "important ";
+    }
+
+    return (
+      <React.Fragment>
+        <span className={className} onClick={this.onLabelClick}>
+          {label}
+        </span>
+        <div className="buttonGroup">
+          <button
+            className="button btn btn-outline-success"
+            onClick={this.onMarkImportant}
+          >
+            <i className="fa fa-exclamation"></i>
+          </button>
+          <button
+            className="button btn btn-outline-danger"
+            onClick={this.onMarkImportant}
+          >
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
-export default ToDoListItem
+export default ToDoListItem;
